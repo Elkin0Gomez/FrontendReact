47599a117968b8d058716e2a307f9e37cad63907
Elkin0Gomez <est_eer_gomez@fesc.edu.co>
2023-10-20 08:12:01 -0500

    se agregan login, 404, 500 y register

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,16 @@ import React from 'react'
 const ListaContratos= React.lazy(()=> import('../components/PartidosCreados/ListaContratos'))
 const crearContrato= React.lazy(()=> import('../components/PartidosCreados/crearContrato'))
 const EditarContrato= React.lazy(()=> import('../components/PartidosCreados/EditarContrato'))
-const Colors = React.lazy(() => import('../views/theme/colors/Colors'))
-const Accordion = React.lazy(() => import('../views/base/accordion/Accordion'))
+const Register= React.lazy(()=> import('../views/pages/register/Register'))
+
 
 const routes = [
   { path: '/', exact: true, name: 'Home'},
   { path: '/listacontratos', name: 'ListaContratos', element: ListaContratos },
   { path: '/crearcontrato', name: 'Crear Contrato', element: crearContrato },
   { path: '/editarContrato/:id', name: 'EditarContrato', element: EditarContrato },
-  { path: '/base/accordion', name: 'Accordion', element: Accordion }
+  { path: '/register', name: 'EditarContrato', element: Register },
+
 ]
 
 export default routes
---
+++ src/views/pages/register/Register.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+
+const Register = () => {
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={9} lg={7} xl={6}>
+            <CCard className="mx-4">
+              <CCardBody className="p-4">
+                <CForm>
+                  <h1>Registrar Usuario</h1>
+                  <p className="text-medium-emphasis">Ingrese los datos del usuario</p>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Usuario" autoComplete="username" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormInput placeholder="Correo" autoComplete="email" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="Contraseña"
+                      placeholder="Password"
+                      autoComplete="new-password"
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-4">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="password"
+                      placeholder="Confirmar contraseña"
+                      autoComplete="new-password"
+                    />
+                  </CInputGroup>
+                  <div className="d-grid">
+                    <CButton color="success">Creatr Usuario</CButton>
+                  </div>
+                </CForm>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Register
